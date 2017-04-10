877503f0288e0090087ea37d65ca7c6edc5a919b
Taylor Laine <tlaine@msudenver.edu>
2017-04-10 10:53:12 -0600

    adds Book model and api/books route

--- models/index.js
+++ models/index.js
@@ -1,2 +1,5 @@
 var mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost/book-app");
+
+// models/index.js
+module.exports.Book = require("./book.js");
--- server.js
+++ server.js
@@ -6,6 +6,9 @@
 //  SETUP and CONFIGURATION
 /////////////////////////////
 
+//require our models from index.js
+var db = require('./models');
+
 //require express in our app
 var express = require('express'),
   bodyParser = require('body-parser');
@@ -25,35 +28,29 @@ app.use(bodyParser.urlencoded({ extended: true }));
 //  DATA
 ///////////////////
 
-var books = [
-  {
-    _id: 15,
-    title: "The Four Hour Workweek",
-    author: "Tim Ferriss",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
-    release_date: "April 1, 2007"
-  },
-  {
-    _id: 16,
-    title: "Of Mice and Men",
-    author: "John Steinbeck",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
-    release_date: "Unknown 1937"
-  },
-  {
-    _id: 17,
-    title: "Romeo and Juliet",
-    author: "William Shakespeare",
-    image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
-    release_date: "Unknown 1597"
-  }
-];
-
-
-
-
-
-
+// var books = [
+//   {
+//     _id: 15,
+//     title: "The Four Hour Workweek",
+//     author: "Tim Ferriss",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/four_hour_work_week.jpg",
+//     release_date: "April 1, 2007"
+//   },
+//   {
+//     _id: 16,
+//     title: "Of Mice and Men",
+//     author: "John Steinbeck",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/of_mice_and_men.jpg",
+//     release_date: "Unknown 1937"
+//   },
+//   {
+//     _id: 17,
+//     title: "Romeo and Juliet",
+//     author: "William Shakespeare",
+//     image: "https://s3-us-west-2.amazonaws.com/sandboxapi/romeo_and_juliet.jpg",
+//     release_date: "Unknown 1597"
+//   }
+// ];
 
 ////////////////////
 //  ROUTES
@@ -70,8 +67,12 @@ app.get('/', function (req, res) {
 // get all books
 app.get('/api/books', function (req, res) {
   // send all books as JSON response
-  console.log('books index');
-  res.json(books);
+  db.Book.find(function(err, books) {
+    //if error return log of that error
+    if (err) { return console.log('index error: ' + err); }
+    // if no error return the books from database to page as json
+    res.json(books);
+  });
 });
 
 // get one book
